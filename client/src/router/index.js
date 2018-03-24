7db9e8f51e36e5e2bf7063856884ef6a18026122
mattias <mattias@stockholmscience.se>
2018-03-24 15:07:06 +0100

    full crud

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
+import EditPost from '@/components/EditPost'
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Hello',
+      component: Hello
     },
     {
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
+    },
+    {
+      path: '/posts/:id',
+      name: 'EditPost',
+      component: EditPost
     }
   ]
 })
--- server/src/app.js
+++ server/src/app.js
@@ -3,22 +3,95 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const Post = require('../models/post')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/posts', (req, res) => {
-    console.log('user trying to acccess page')
-    res.send(
-        [{
-            title: "Hello there!",
-            description: "We are the knights who say Ni!"
-        }]
-    )
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://greencow:hubbabubba@ds123029.mlab.com:23029/1dv430')
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', function (callback) {
+  console.log('Connection Succeeded')
 })
 
-app.listen(process.env.PORT || 8081, function() {
-    console.log('Server started on port 8081; press Ctrl-C to terminate')
+// Add new post
+app.post('/posts', (req, res) => {
+  var db = req.db
+  var title = req.body.title
+  var description = req.body.description
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
+// Fetch all posts
+app.get('/posts', (req, res) => {
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error) }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id: -1})
+})
+
+// Fetch single post
+app.get('/post/:id', (req, res) => {
+  var db = req.db
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error) }
+    res.send(post)
+  })
+})
+
+// Update a post
+app.put('/posts/:id', (req, res) => {
+  var db = req.db
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error) }
+
+    post.title = req.body.title
+    post.description = req.body.description
+    post.save(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      res.send({
+        success: true
+      })
+    })
+  })
+})
+
+// Delete a post
+app.delete('/posts/:id', (req, res) => {
+  var db = req.db;
+  Post.remove({
+    _id: req.params.id
+  }, function (err, post) {
+    if (err) {
+      res.send(err)
+    }
+    res.send({
+      success: true
+    })
+  })
+})
+
+app.listen(process.env.PORT || 8081, function () {
+  console.log('Server started on port 8081; press Ctrl-C to terminate')
 })
